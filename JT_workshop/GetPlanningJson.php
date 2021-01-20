8b7652c50e86afc251ca3146f9e18e0f8e9ebc2b
Olivier <olivier@pop-os.localdomain>
2021-01-20 16:59:40 +0400

    step4

--- JT_workshop/GetPlanningJson.php
+++ JT_workshop/GetPlanningJson.php
@@ -15,9 +15,21 @@ $CompleteAutologin = "https://intra.epitech.eu/$autologin/planning/load?format=j
 /////STEP3////
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $CompleteAutologin);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
+$JSONAsString = curl_exec($ch);
 curl_close($ch);
 /////STEP3////
+
+/////STEP4////
+$data = json_decode($JSONAsString, true);
+$ConfigAsSTring = file_get_contents('./config.json');
+$model = json_decode($ConfigAsSTring, true);
+$myconfig = $model["SOMETHING"];
+
+for ($i = 0; $i != sizeof($data); $i++) {
+    echo $data[$i][$myconfig], "\n";
+}
+/////STEP4/////
 
 ?>
