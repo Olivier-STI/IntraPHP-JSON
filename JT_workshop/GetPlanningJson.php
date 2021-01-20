dbe828a22d4ee8bb918c22c818007a6ba4e07f8c
Olivier <olivier@pop-os.localdomain>
2021-01-20 15:57:06 +0400

    Step3

--- JT_workshop/GetPlanningJson.php
+++ JT_workshop/GetPlanningJson.php
@@ -1,14 +1,23 @@
 #!/usr/bin/php
 <?php
+////STEP2////
 $autologin = file_get_contents('./autologin');
 $ActualYears = date('Y');
 $ActualMonth = date('m');
 $LastMonday = date('d', strtotime("Monday this week"));
 $NextSunday = date('d', strtotime("Sunday this week"));
-
 $Start = "$ActualYears-$ActualMonth-$LastMonday";
 $End = "$ActualYears-$ActualMonth-$NextSunday";
-
 $CompleteAutologin = "https://intra.epitech.eu/$autologin/planning/load?format=json&start=$Start&end=$End";
-echo $CompleteAutologin;
+////STEP2////
+
+
+/////STEP3////
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $CompleteAutologin);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_exec($ch);
+curl_close($ch);
+/////STEP3////
+
 ?>
